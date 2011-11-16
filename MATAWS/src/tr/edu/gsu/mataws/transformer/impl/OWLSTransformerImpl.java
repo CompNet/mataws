0d174e720b0be621d22623ae3e53edd5234c1599
cihanaxoy <cihanaxoy@80949dbf-bf2c-7204-d78c-b3de6bd5de4a>
2011-11-16 21:25:26 +0000

    Initial import.

---
+++ MATAWS/src/tr/edu/gsu/mataws/transformer/impl/OWLSTransformerImpl.java
@@ -0,0 +1,197 @@
+/*
+ * Mataws - Multimodal Automatic Tool for the Annotation of  Web Services
+ * Copyright 2011 Cihan Aksoy & Koray Mançuhan
+ * 
+ * This file is part of Mataws - Multimodal Automatic Tool for the Annotation of  Web Services.
+ * 
+ * Mataws - Multimodal Automatic Tool for the Annotation of  Web Services is 
+ * free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * Mataws - Multimodal Automatic Tool for the Annotation of  Web Services 
+ * is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Mataws - Multimodal Automatic Tool for the Annotation of  Web Services.
+ * If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+package tr.edu.gsu.mataws.transformer.impl;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Vector;
+
+import org.mindswap.owl.OWLFactory;
+import org.mindswap.owl.OWLOntology;
+import org.mindswap.owls.vocabulary.OWLS;
+import org.mindswap.wsdl.WSDLOperation;
+import org.mindswap.wsdl.WSDLParameter;
+import org.mindswap.wsdl.WSDLService;
+import org.mindswap.wsdl.WSDLTranslator;
+
+import tr.edu.gsu.mataws.statistics.Statistics;
+import tr.edu.gsu.mataws.statistics.impl.StatisticsUtil;
+import tr.edu.gsu.mataws.transformer.Transformer;
+
+/**
+ * This class is used to transform a wsdl file to owl-s file.
+ * 
+ * @author Cihan Aksoy
+ *
+ */
+public class OWLSTransformerImpl implements Transformer {
+
+	private Statistics statistics;
+	private Map<String, List<String>> parameterAnnotationMap;
+	private String prefix = "#";
+	
+	/**
+	 * Constructs an instance of this class.
+	 */
+	public OWLSTransformerImpl(){
+		statistics = StatisticsUtil.getInstance();
+		parameterAnnotationMap = statistics.getParameterAnnotationMap();
+	}
+	
+	@Override
+	public void transform(String sourceFile, String destFolder) {
+		
+		WSDLService s = null;
+		List<WSDLOperation> ops = new ArrayList<WSDLOperation>();
+		
+		File sFile = new File(sourceFile);
+		
+		//wsdl loading
+		try
+		{
+			s = WSDLService.createService(sFile.toURI().toString());
+			ops = s.getOperations();
+		} catch(Exception e){
+			e.printStackTrace();
+		}
+		
+		for (WSDLOperation wsdlOperation : ops) {
+			if(wsdlOperation == null){
+				System.out.println("WSDL operation is null !!");
+			} else{
+				String serviceName = wsdlOperation.getName();
+				String name = serviceName.replaceAll(" ", "_");
+				
+				File file = new File(destFolder + File.separator + name + ".owl");
+				
+				URI ontURI = null;
+				try
+				{
+					ontURI = new URI("http://www.gsu.edu.tr/service.owl");
+				}
+				catch (URISyntaxException e)
+				{
+					e.printStackTrace();
+				}
+				
+				OWLOntology ont = OWLFactory.createKB().createOntology(ontURI);
+				OWLS.addOWLSImports(ont);
+				
+				WSDLTranslator t = new WSDLTranslator(ont, wsdlOperation, name);
+				t.setServiceName(serviceName);
+				t.setTextDescription(wsdlOperation.getDescription());
+				
+				//inputs
+				Vector<WSDLParameter> inputs = wsdlOperation.getInputs();
+				for (WSDLParameter wsdlParameter : inputs) {
+					String paramName = wsdlParameter.getName();
+					String[] paramNameTemp = paramName.split("#");
+					List<String> concepts = new ArrayList<String>();
+					String paramType = null;
+					
+					concepts = parameterAnnotationMap.get(paramNameTemp[1]);
+					
+					if(concepts != null){
+						if(concepts.size()>0)
+							paramType = prefix + concepts.get(0);
+						else
+							paramType = prefix + "NoMatch";
+					}else
+						paramType = prefix + "NoMatch";
+					
+					URI paramTypeURI = null;
+					try
+					{
+						paramTypeURI = new URI(paramType);
+					}
+					catch (IllegalArgumentException e)
+					{
+						e.printStackTrace();
+					} catch (URISyntaxException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					String xsltTransformation = "xsltTransformation";
+					
+					t.addInput(wsdlParameter, paramName, paramTypeURI, xsltTransformation);
+				}
+				
+				//outputs
+				Vector<WSDLParameter> outputs = wsdlOperation.getOutputs();
+				for (WSDLParameter wsdlParameter : outputs) {
+					String paramName = wsdlParameter.getName();
+					String[] paramNameTemp = paramName.split("#");
+					List<String> concepts = new ArrayList<String>();
+					String paramType = null;
+					
+					concepts = parameterAnnotationMap.get(paramNameTemp[1]);
+					
+					if(concepts != null){
+						if(concepts.size()>0)
+							paramType = prefix + concepts.get(0);
+						else
+							paramType = prefix + "NoMatch";
+					}else
+						paramType = prefix + "NoMatch";
+					
+					URI paramTypeURI = null;
+					try
+					{
+						paramTypeURI = new URI(paramType);
+					}
+					catch (IllegalArgumentException e)
+					{
+						e.printStackTrace();
+					} catch (URISyntaxException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					String xsltTransformation = "xsltTransformation";
+					
+					t.addOutput(wsdlParameter, paramName, paramTypeURI, xsltTransformation);
+				}
+				
+				try
+				{
+					FileOutputStream fos = new FileOutputStream(file);
+					t.writeOWLS(fos);
+					fos.close();
+					System.out.println("Saved to " + file.toURI());
+				}
+				catch (Exception e)
+				{
+					e.printStackTrace();
+				}
+				
+			}
+		}
+		
+	}
+
+}
