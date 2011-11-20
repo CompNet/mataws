0ae51c152d8f54daf86a36ccd535e314139ed90b
cihanaxoy <cihanaxoy@80949dbf-bf2c-7204-d78c-b3de6bd5de4a>
2011-11-20 22:15:10 +0000

    Analyzer package for selecting one of the given words to annotate.

---
+++ src/tr/edu/gsu/mataws/analyzer/AnalysisType.java
@@ -0,0 +1,5 @@
+package tr.edu.gsu.mataws.analyzer;
+
+public enum AnalysisType {
+	NonNounAnnotation, OnlyOneRemaining, OnlyOneRepresenter, HypernymialRelation, HolonymialRelation, NounAdjunct, NoAnalysis
+}
