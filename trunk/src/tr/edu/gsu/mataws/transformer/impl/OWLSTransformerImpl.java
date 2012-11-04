85b165d91c2d3344eb9f7f2ed3a5beda0f62c08d
cihanaxoy <cihanaxoy@80949dbf-bf2c-7204-d78c-b3de6bd5de4a>
2012-11-04 18:43:59 +0000

    class was formatted.

--- trunk/src/tr/edu/gsu/mataws/transformer/impl/OWLSTransformerImpl.java
+++ trunk/src/tr/edu/gsu/mataws/transformer/impl/OWLSTransformerImpl.java
@@ -47,150 +47,142 @@ import tr.edu.gsu.mataws.transformer.Transformer;
  * This class is used to transform a wsdl file to owl-s file.
  * 
  * @author Cihan Aksoy
- *
+ * 
  */
 public class OWLSTransformerImpl implements Transformer {
 
 	private StatisticsUtil statistics;
 	private Map<String, String> parameterAnnotationMap;
 	private String prefix = "#";
-	
+
 	/**
 	 * Constructs an instance of this class.
 	 */
-	public OWLSTransformerImpl(){
+	public OWLSTransformerImpl() {
 		statistics = StatisticsUtil.getInstance();
 		parameterAnnotationMap = statistics.getParameterAnnotationMap();
 	}
-	
+
 	@Override
 	public void transform(String sourceFile, String destFolder) {
-		
+
 		WSDLService s = null;
 		List<WSDLOperation> ops = new ArrayList<WSDLOperation>();
-		
+
 		File sFile = new File(sourceFile);
-		
-		//wsdl loading
-		try
-		{
+
+		// wsdl loading
+		try {
 			s = WSDLService.createService(sFile.toURI().toString());
 			ops = s.getOperations();
-		} catch(Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		for (WSDLOperation wsdlOperation : ops) {
-			if(wsdlOperation == null){
+			if (wsdlOperation == null) {
 				System.out.println("WSDL operation is null !!");
-			} else{
+			} else {
 				String serviceName = wsdlOperation.getName();
 				String name = serviceName.replaceAll(" ", "_");
-				
-				File file = new File(destFolder + File.separator + name + ".owl");
-				
+
+				File file = new File(destFolder + File.separator + name
+						+ ".owl");
+
 				URI ontURI = null;
-				try
-				{
+				try {
 					ontURI = new URI("http://www.gsu.edu.tr/service.owl");
-				}
-				catch (URISyntaxException e)
-				{
+				} catch (URISyntaxException e) {
 					e.printStackTrace();
 				}
-				
+
 				OWLOntology ont = OWLFactory.createKB().createOntology(ontURI);
 				OWLS.addOWLSImports(ont);
-				
+
 				WSDLTranslator t = new WSDLTranslator(ont, wsdlOperation, name);
 				t.setServiceName(serviceName);
 				t.setTextDescription(wsdlOperation.getDescription());
-				
-				//inputs
+
+				// inputs
 				Vector<WSDLParameter> inputs = wsdlOperation.getInputs();
 				for (WSDLParameter wsdlParameter : inputs) {
 					String paramName = wsdlParameter.getName();
 					String[] paramNameTemp = paramName.split("#");
 					String concept = null;
 					String paramType = null;
-					
+
+					// since wsdlParameter type doesn't give the parameter
+					// itself, we find a concept for a param using its name
 					concept = parameterAnnotationMap.get(paramNameTemp[1]);
-					
-					if(concept != null){
-						if(!concept.equals("NoMatch"))
+
+					if (concept != null) {
+						if (!concept.equals("NoMatch"))
 							paramType = prefix + concept;
 						else
 							paramType = prefix + "NoMatch";
-					}else
+					} else
 						paramType = prefix + "NoMatch";
-					
+
 					URI paramTypeURI = null;
-					try
-					{
+					try {
 						paramTypeURI = new URI(paramType);
-					}
-					catch (IllegalArgumentException e)
-					{
+					} catch (IllegalArgumentException e) {
 						e.printStackTrace();
 					} catch (URISyntaxException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
 					String xsltTransformation = "xsltTransformation";
-					
-					t.addInput(wsdlParameter, paramName, paramTypeURI, xsltTransformation);
+
+					t.addInput(wsdlParameter, paramName, paramTypeURI,
+							xsltTransformation);
 				}
-				
-				//outputs
+
+				// outputs
 				Vector<WSDLParameter> outputs = wsdlOperation.getOutputs();
 				for (WSDLParameter wsdlParameter : outputs) {
 					String paramName = wsdlParameter.getName();
 					String[] paramNameTemp = paramName.split("#");
 					String concept = null;
 					String paramType = null;
-					
+
 					concept = parameterAnnotationMap.get(paramNameTemp[1]);
-					
-					if(concept != null){
-						if(!concept.equals("NoMatch"))
+
+					if (concept != null) {
+						if (!concept.equals("NoMatch"))
 							paramType = prefix + concept;
 						else
 							paramType = prefix + "NoMatch";
-					}else
+					} else
 						paramType = prefix + "NoMatch";
-					
+
 					URI paramTypeURI = null;
-					try
-					{
+					try {
 						paramTypeURI = new URI(paramType);
-					}
-					catch (IllegalArgumentException e)
-					{
+					} catch (IllegalArgumentException e) {
 						e.printStackTrace();
 					} catch (URISyntaxException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
 					String xsltTransformation = "xsltTransformation";
-					
-					t.addOutput(wsdlParameter, paramName, paramTypeURI, xsltTransformation);
+
+					t.addOutput(wsdlParameter, paramName, paramTypeURI,
+							xsltTransformation);
 				}
-				
-				try
-				{
+
+				try {
 					FileOutputStream fos = new FileOutputStream(file);
 					t.writeOWLS(fos);
 					fos.close();
 					System.out.println("Saved to " + file.toURI());
-				}
-				catch (Exception e)
-				{
+				} catch (Exception e) {
 					e.printStackTrace();
 				}
-				
+
 			}
 		}
-		
+
 	}
 
 }
