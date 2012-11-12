c86367b5a614019f99dfd8d1640f2956a08f3225
cihanaxoy <cihanaxoy@80949dbf-bf2c-7204-d78c-b3de6bd5de4a>
2012-11-12 11:46:16 +0000

    version-1.4.0

--- src/tr/edu/gsu/mataws/components/CsvObjectForParams.java
+++ src/tr/edu/gsu/mataws/components/CsvObjectForParams.java
@@ -1,3 +1,26 @@
+/*
+ * Mataws - Multimodal Automatic Tool for the Annotation of  Web Services
+ * Copyright 2011 Cihan Aksoy & Koray Mançuhan
+ * 
+ * This file is part of Mataws - Multimodal Automatic Tool for the Annotation of  Web Services.
+ * 
+ * Mataws - Multimodal Automatic Tool for the Annotation of  Web Services is 
+ * free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * Mataws - Multimodal Automatic Tool for the Annotation of  Web Services 
+ * is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Mataws - Multimodal Automatic Tool for the Annotation of  Web Services.
+ * If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
 package tr.edu.gsu.mataws.components;
 
 public class CsvObjectForParams {
